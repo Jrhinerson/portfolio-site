4f758691cb4a4b26bc3680f22c5bc8b7e3a34e80
Jordan Rhinerson <jordan.rhinerson@gmail.com>
2023-07-24 13:43:50 -0400

    Update Navbar and create personal logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full h-[80px] bg-darkteal flex justify-between items-center p-4'>
+    <div className='fixed w-full h-[80px] bg-darkteal text-lightgray flex justify-between items-center px-4'>
         {/* Logo */}
         <div>
             <img />
         </div>
         {/* Menu Items */}
-        <ul className='text-lightgray'>
+        <ul className='flex text-lg font-semibold' >
             <li>Home</li>
             <li>About</li>
             <li>Work</li>
